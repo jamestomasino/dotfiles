e3b97880165266d10aa5ce6c9c497453f7b9006f
James Tomasino <james@tomasino.org>
2016-12-29 18:20:41 -0500

    system configuration tweaks

--- osxsetup.sh
+++ osxsetup.sh
@@ -204,78 +204,119 @@ $HOME/.tmux/plugins/tpm/bin/install_plugins
 ####################### System Configuration ###################################
 ################################################################################
 
-#Set a blazingly fast keyboard repeat rate
+# Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0.02
 
-#Set a shorter Delay until key repeat
+# Set a shorter Delay until key repeat
 defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
-#Add a context menu item for showing the Web Inspector in web views
+# Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-#Show the ~/Library folder
+# Show the ~/Library folder
 chflags nohidden ~/Library
 
-#Store screenshots in subfolder on desktop
+# Store screenshots in subfolder on desktop
 mkdir ~/Desktop/Screenshots
 defaults write com.apple.screencapture location ~/Desktop/Screenshots
 
-#Disable the 'Are you sure you want to open this application?' dialog
+# Disable the 'Are you sure you want to open this application?' dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-#Show percentage in battery status
+# Show percentage in battery status
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 defaults write com.apple.menuextra.battery ShowTime -string "NO"
 
-#Disable Notification Center and remove the menu bar icon
+# Disable Notification Center and remove the menu bar icon
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
 
-#Disable rubberband scrolling
+# Disable rubberband scrolling
 defaults write -g NSScrollViewRubberbanding -bool false
 
-#Disable dashboard
+# Disable dashboard
 defaults write com.apple.dashboard mcx-disabled -boolean YES
 
-#Move dock to left side of screen
+# Move dock to left side of screen
 defaults write com.apple.dock orientation -string left
 
-#Show all filename extensions in Finder
+# Show all filename extensions in Finder
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-#Expand save panel by default
+# Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-#Expand print panel by default
+# Expand print panel by default
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 
-#Avoid creating .DS_Store files on network volumes
+# Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-#Disable the warning when changing a file extension
+# Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-#Use list view in all Finder windows by default
-#Four-letter codes for the other view modes: icnv, Nlmv, Flwv
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: icnv, Nlmv, Flwv
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlmv"
 
-#Remove default text from basic screen saver
+# Remove default text from basic screen saver
 defaults write ~/Library/Preferences/com.apple.ScreenSaver.Basic MESSAGE " "
 
-#Disable sound effect when changing volume
+# Disable sound effect when changing volume
 defaults write -g com.apple.sound.beep.feedback -integer 0
 
-#Disable user interface sound effects
+# Disable user interface sound effects
 defaults write com.apple.systemsound 'com.apple.sound.uiaudio.enabled' -int 0
 
-#Set system sounds volume to 0
+# Set system sounds volume to 0
 defaults write com.apple.systemsound com.apple.sound.beep.volume -float 0
 
-#Autohide Dock
+# Autohide Dock
 defaults write com.apple.dock autohide -bool true
 
-#Show/Hide Dock instantly
+# Show/Hide Dock instantly
 defaults write com.apple.Dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
 
-#Kill affected applications, so the changes apply
+# Disable Smart Quotes 
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable Smart Dashes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Set Desktop as the default location for new Finder windows
+# For other paths, use `PfLo` and `file:///full/path/here/`
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Show item info to the right of the icons on the desktop
+/usr/libexec/PlistBuddy -c "Set DesktopViewSettings:IconViewSettings:labelOnBottom false" ~/Library/Preferences/com.apple.finder.plist
+
+# Enable snap-to-grid for icons on the desktop and in other icon views 
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+# Ask for admin password upfront
+sudo -v
+
+# Keepalive for sudo until done
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Set volume to 0 at boot
+sudo nvram SystemAudioVolume=" "
+
+# Set default desktop background
+curl "https://s3.amazonaws.com/squishyface/images/background.jpg" > "background.jpg"
+sudo mv "background.jpg" "/Library/Desktop Pictures/dune.jpg"
+rm -rf "$HOME/Library/Application\ Support/Dock/desktoppicture.db"
+sudo rm -rf "/System/Library/CoreServices/DefaultDesktop.jpg"
+sudo ln -s "/Library/Desktop Pictures/dune.jpg" "/System/Library/CoreServices/DefaultDesktop.jpg"
+
+# Kill affected applications, so the changes apply
 for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
