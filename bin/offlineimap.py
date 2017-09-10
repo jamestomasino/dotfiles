0cd434501e99ef3e90d7644cc0100e2d10b97e0d
James Tomasino <james@tomasino.org>
2017-09-09 21:21:44 -0400

    cleaning up bins

--- bin/offlineimap.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import re, subprocess
-def get_keychain_pass(server=None):
-    command = "lpass show --password %s" % server
-    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-    return output.replace('\n', '').replace('\r', '')
